e31f8b9bce60e51e04eb787a382e6057820d5045
Fionn E-M <86577204+EmrysAlbiorix@users.noreply.github.com>
2024-03-05 10:53:34 -0800

    Add files via upload

--- CS467/Projects/zBuffer.c
+++ CS467/Projects/zBuffer.c
@@ -1,57 +1,9 @@
-// read the data from multiple xyz files and display all of the objects
-// Packages
 #include "Tools/FPToolkit.c"
 #include "Tools/M3d_matrix_tools.c"
 
-// Global variables
-int window_width, window_height, window_square_size ;
-double Half_window_size ;
-double Half_angle_degrees ;
-double Tan_half_angle ;
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-// M controls the maximum number of objects :
-#define M 30
-int numobjects ;
-int numpoints[M] ;
-int numpolys[M] ;
-double x[M][10000] ;
-double y[M][10000] ;
-double z[M][10000] ;
-int psize[M][5000] ;
-int con[M][5000][20] ; // no polygon more than 20 verts
-double inherent_rgb[M][3] ;
-
-
-// Reads the XYZ files
-int read_xyz_file_into_data_structure (FILE *f, int onum) {
-  int i,polynum,j ;
-
-  // point info
-  fscanf(f,"%d",&numpoints[onum]) ;
-  for (i = 0 ; i < numpoints[onum] ; i++) {
-    fscanf(f,"%lf %lf %lf",&x[onum][i],&y[onum][i],&z[onum][i]) ;
-  }
-
-
-  // connectivity info
-  fscanf(f,"%d",&numpolys[onum]) ;
-  for (polynum = 0 ; polynum < numpolys[onum] ; polynum++) {
-
-    fscanf(f,"%d",&psize[onum][polynum]) ;
-
-    for (j = 0 ; j < psize[onum][polynum] ; j++) {
-      fscanf(f,"%d",&con[onum][polynum][j]) ;
-    }
-  }
-
-}
-
-
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+double zBuffer[800][800] ;
+double coi[3], eye[3], up[3] ;
 
 
 // To support the light model :
@@ -59,9 +11,9 @@ double light_in_eye_space[3] ;
 double AMBIENT      = 0.2 ;
 double MAX_DIFFUSE  = 0.5 ;
 double SPECPOW      = 50 ;
+double inherent_rgb[3];
 
 
-// Light Model
 int Light_Model (double irgb[3],
                  double s[3],
                  double p[3],
@@ -85,6 +37,7 @@ int Light_Model (double irgb[3],
   if (len == 0) return 0 ;
   N[0] = n[0]/len ;  N[1] = n[1]/len ;  N[2] = n[2]/len ;
 
+
   double E[3] ;
   E[0] = s[0] - p[0] ; 
   E[1] = s[1] - p[1] ; 
@@ -93,6 +46,7 @@ int Light_Model (double irgb[3],
   if (len == 0) return 0 ;
   E[0] /= len ;  E[1] /= len ;  E[2] /= len ;
   double NdotE = N[0]*E[0] + N[1]*E[1] + N[2]*E[2] ;
+
 
   double L[3] ;
   L[0] = light_in_eye_space[0] - p[0] ; 
@@ -162,7 +116,6 @@ int Light_Model (double irgb[3],
 }
 
 
-// Applies light model
 void light_model (double irgb[3],
                   double *xx, double *yy, double *zz, 
                   double argb[3])
@@ -189,312 +142,9 @@ void light_model (double irgb[3],
 }
 
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-// To support Painter's algorithm :
-typedef
-struct {
-  int onum ;
-  int pnum ;
-  double dist ;
-}
-PINFO ;
-
-PINFO polyinfo[20000] ;
-
-int PINFO_compare (const void *p, const void *q)
-{
-  PINFO *a, *b ;
-
-  a = (PINFO*)p ;
-  b = (PINFO*)q ;
-
-  // remember we want furthest distance FIRST
-  if      (((*a).dist) < ((*b).dist)) return  1 ;
-  else if (((*a).dist) > ((*b).dist)) return -1 ;
-  else                                return  0 ;
-}
-
-
-// Draws objects
-void draw_all_objects()
-{
-  double a,b,c ;
-  int onum, pnum,j,size,d ;
-  double xp[100],yp[100] ;
-
-  int TotalPolys ;
-  double sumx,sumy,sumz ;
-
-  a = Half_window_size / Tan_half_angle ;
-  b = 0.5*window_width ;
-  c = 0.5*window_height ;
-
-  TotalPolys = 0 ;
-
-  for (onum = 0 ; onum < numobjects ; onum++) {
-
-      for (pnum = 0 ; pnum < numpolys[onum] ; pnum++) {
-
-        size = psize[onum][pnum] ;
-
-	sumx = sumy = sumz = 0.0 ;
-        for (j = 0 ; j < size ; j++) {
-          d = con[onum][pnum][j] ;
-
-          sumx += x[onum][d] ;
-          sumy += y[onum][d] ;
-          sumz += z[onum][d] ;
-
-        }
-	sumx /= size ;
-	sumy /= size ;
-	sumz /= size ;
-	polyinfo[TotalPolys].onum = onum ;
-	polyinfo[TotalPolys].pnum = pnum ;
-	polyinfo[TotalPolys].dist = sqrt(sumx*sumx + sumy*sumy + sumz*sumz) ;
-	TotalPolys++ ;
-
-      } // end for pnum
-
-  } // end for onum
-
-
-  // printf("TotalPolys = %d\n",TotalPolys) ;
-  ///////////////////////////////////////
-  qsort(polyinfo, TotalPolys, sizeof(PINFO), PINFO_compare) ;
-  ///////////////////////////////////////
-
-  double xx[100],yy[100],zz[100] ;
-  double rgb[3] ;
-  int k ;
-  for (k = 0 ; k < TotalPolys ; k++) {
-
-    onum = polyinfo[k].onum ;
-    pnum = polyinfo[k].pnum ;
-
-        size = psize[onum][pnum] ;
-
-        for (j = 0 ; j < size ; j++) {
-          d = con[onum][pnum][j] ;
-
-          xx[j] = x[onum][d] ;
-          yy[j] = y[onum][d] ;
-          zz[j] = z[onum][d] ;
-
-          xp[j] = a*(xx[j]/zz[j]) + b ;
-          yp[j] = a*(yy[j]/zz[j]) + c ;
-        }
-
-
-        light_model (inherent_rgb[onum], xx,yy,zz, rgb) ;
-
-	G_rgb(rgb[0], rgb[1], rgb[2]) ;
-	// printf("%lf %lf %lf\n",
-	// inherent_rgb[onum][0],inherent_rgb[onum][2],inherent_rgb[onum][2]) ;
-        // printf("%lf %lf %lf\n",rgb[0],rgb[1],rgb[2]) ;
-
-        G_fill_polygon(xp,yp,size) ;
-	G_rgb(0,0,0) ;
-	// G_polygon(xp,yp,size) ;
-
-  }
-
-}
-
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
-int main ()
-{
-  FILE *f ;
-  double V[4][4], Vi[4][4] ;
-  int onum, q ;
 
-  int nl ;
-  int tlist[100] ;
-  double plist[100] ;
-
-
-
-  window_width = 600 ; window_height = 600 ;
-  // size of largest square INside window
-  if (window_width < window_height) { window_square_size = window_width ; }
-                               else { window_square_size = window_height ; }
-  Half_window_size = 0.5*window_square_size ;
-  Half_angle_degrees = 30 ;
-  Tan_half_angle = tan(Half_angle_degrees*M_PI/180) ;
-  G_init_graphics (window_width, window_height) ;
-
-
-
-  light_in_eye_space[0] =  100 ;
-  light_in_eye_space[1] =  100 ;
-  light_in_eye_space[2] = -100 ;
-  
-  AMBIENT = 0.2 ;
-  MAX_DIFFUSE = 0.5 ;
-  SPECPOW = 30 ;
-
-
-
-
-  //---------------------------------------------------------------------
-  // Design the model by moving objects from their own OBJECT SPACE 
-  // to a common WORLD SPACE :
-  //---------------------------------------------------------------------
-
-  onum = 0 ;  // current object number
-
-  // The sphere FILE has a radius of 1.0, centered at the origin.
-  // The cylinder FILE has radius of 0.1, centered at origin, 
-  // lying on the x-axis from x = -1 to x = 1.
-
-
-  // Build an origin point out of the sphere file.
-  f = fopen("XYZ/sphere.xyz","r") ;
-  if (f == NULL) { printf("Can't open file %s\n", "sphere.xyz") ;  exit(0) ;  }
-  inherent_rgb[onum][0] = 1.0 ;
-  inherent_rgb[onum][1] = 0.8 ;
-  inherent_rgb[onum][2] = 0.0 ;
-  read_xyz_file_into_data_structure (f, onum) ;
-  fclose(f) ;
-
-  nl = 0 ;
-  tlist[nl] = SX ; plist[nl] = 0.25 ; nl++ ;
-  tlist[nl] = SY ; plist[nl] = 0.25 ; nl++ ;
-  tlist[nl] = SZ ; plist[nl] = 0.25 ; nl++ ;  
-  M3d_make_movement_sequence_matrix (V,Vi,  nl,tlist,plist) ;
-
-  M3d_mat_mult_points (x[onum],y[onum],z[onum],
-		       V,  x[onum],y[onum],z[onum],numpoints[onum]) ;
-
-  onum++ ;
-
-
-  // Build a +x axis with the cylinder file.
-  f = fopen("XYZ/cylinder.xyz","r") ;
-  if (f == NULL) { printf("Can't open file %s\n", "cylinder.xyz") ;  exit(0) ;  }
-  inherent_rgb[onum][0] = 1.0 ;
-  inherent_rgb[onum][1] = 0.2 ;
-  inherent_rgb[onum][2] = 0.2 ;
-  read_xyz_file_into_data_structure (f, onum) ;
-  fclose(f) ;
-
-  nl = 0 ;
-  tlist[nl] = TX ; plist[nl] = 1.00 ; nl++ ;
-  tlist[nl] = SX ; plist[nl] = 2.00 ; nl++ ;
-  M3d_make_movement_sequence_matrix (V,Vi,  nl,tlist,plist) ;
-  
-  M3d_mat_mult_points (x[onum],y[onum],z[onum],
-		       V,  x[onum],y[onum],z[onum],numpoints[onum]) ;
-
-  onum++ ;
-
-
-  // Build a +y axis with the cylinder file.
-  f = fopen("XYZ/cylinder.xyz","r") ;
-  if (f == NULL) { printf("Can't open file %s\n", "cylinder.xyz") ;  exit(0) ;  }
-  inherent_rgb[onum][0] = 1.0 ;
-  inherent_rgb[onum][1] = 1.0 ;
-  inherent_rgb[onum][2] = 1.0 ;
-  read_xyz_file_into_data_structure (f, onum) ;
-  fclose(f) ;
-  
-  nl = 0 ;
-  tlist[nl] = TX ; plist[nl] = 1.00 ; nl++ ;
-  tlist[nl] = SX ; plist[nl] = 2.00 ; nl++ ;
-  tlist[nl] = RZ ; plist[nl] =  90  ; nl++ ;
-  M3d_make_movement_sequence_matrix (V,Vi,  nl,tlist,plist) ;
-  
-  M3d_mat_mult_points (x[onum],y[onum],z[onum],
-		       V,  x[onum],y[onum],z[onum],numpoints[onum]) ;
-
-  onum++ ;
-
-
-  // Build a +z axis with a cylinder.
-  f = fopen("XYZ/cylinder.xyz","r") ;
-  if (f == NULL) { printf("Can't open file %s\n", "cylinder.xyz") ;  exit(0) ;  }
-  inherent_rgb[onum][0] = 0.3 ;
-  inherent_rgb[onum][1] = 0.2 ;
-  inherent_rgb[onum][2] = 1.0 ;
-  read_xyz_file_into_data_structure (f, onum) ;
-  fclose(f) ;
-
-  nl = 0 ;
-  tlist[nl] = TX ; plist[nl] = 1.00 ; nl++ ;
-  tlist[nl] = SX ; plist[nl] = 2.00 ; nl++ ;
-  tlist[nl] = RY ; plist[nl] = -90  ; nl++ ;
-  M3d_make_movement_sequence_matrix (V,Vi,  nl,tlist,plist) ;  
-
-  M3d_mat_mult_points (x[onum],y[onum],z[onum],
-		       V,  x[onum],y[onum],z[onum],numpoints[onum]) ;
-
-  onum++ ;
-
-
-
-  //---------------------------------------------------------------------
-  numobjects = onum ;
-  //---------------------------------------------------------------------
-
-  double eye[3], coi[3], up[3] ;
-
-  int fnum ;
-  double t ;
-
-  fnum = 0 ;
-
-  while (q != 'q') {
-int initGraphics(int x, int y) {
-  G_init_graphics(x, y);
-  G_rgb(0, 0, 0);
-  G_clear();
-  G_rgb(1, 1, 1);
-  return 0;
-}
-    t = 0.01*fnum ;
-
-    eye[0] = 15*cos(2*M_PI*t) ; 
-    eye[1] =  6*t ; 
-    eye[2] =  7*sin(2*M_PI*t) ; 
-
-    // printf("t = %lf   eye = %lf %lf %lf\n",t, eye[0],eye[1],eye[2]) ;
-
-    coi[0] =  1.0 ;
-    coi[1] =  2.0 ; 
-    coi[2] =  0.5 ;
-
-    up[0]  = eye[0] ; 
-    up[1]  = eye[1] + 1 ;
-    up[2]  = eye[2] ; 
-
-    // put your code here!!!!!!!!!!!!
-    double v[4][4];
-    double vi[4][4];
-    M3d_view(v, vi, eye, coi, up);
-
-    for (int i = 0; i < numobjects; i++) {
-      M3d_mat_mult_points(x[i], y[i], z[i], v, x[i], y[i], z[i], numpoints[i]);
-    }
-
-    G_rgb(0, 0, 0);
-    G_clear();
-    draw_all_objects();
-    G_display_image();
-
-    for (int i = 0; i < numobjects; i++) {
-      M3d_mat_mult_points(x[i], y[i], z[i], vi, x[i], y[i], z[i], numpoints[i]);
-    }
-
-    fnum++;
-    q = G_wait_key();
-  }  // end while (1)
-
-  G_close();
-}
